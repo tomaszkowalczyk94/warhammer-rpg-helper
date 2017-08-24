c426c3d5ef1b8a312e84213d91bbe0e870ab7783
Tomasz Kowalczyk <tomasz.kowalczyk94@gmail.com>
2017-08-24 22:30:20 +0200

    gui - connect to server and create server

--- src/main/java/warhammerrpg/gui/network/ChooseServerForm.java
+++ src/main/java/warhammerrpg/gui/network/ChooseServerForm.java
@@ -1,9 +1,48 @@
 package warhammerrpg.gui.network;
 
-import javax.swing.*;
-import java.awt.*;
+import warhammerrpg.network.Client;
+import warhammerrpg.network.Server;
+import warhammerrpg.network.exception.ClientConnectException;
+import warhammerrpg.network.exception.NetworkException;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class ChooseServerForm {
+
+    private Client client;
+    private Server server;
+
+    public ChooseServerForm() {
+        connectButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+                    client = new Client(ipConnectText.getText(), Integer.parseInt(portConnectText.getText()) );
+                    JOptionPane.showMessageDialog(panel, "Połączono");
+                } catch (ClientConnectException exception) {
+                    JOptionPane.showMessageDialog(panel, "Nie można połączyć się z serwerem", "błąd", JOptionPane.ERROR_MESSAGE);
+                } catch(NumberFormatException exception) {
+                    JOptionPane.showMessageDialog(panel, "Niepoprawny port", "błąd", JOptionPane.ERROR_MESSAGE);
+                }
+
+            }
+        });
+        createGameButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                server = new Server();
+                try {
+                    server.run(Integer.parseInt(portCreateText.getText()));
+                    JOptionPane.showMessageDialog(panel, "Serwer uruchomiony");
+                } catch (NetworkException e1) {
+                    JOptionPane.showMessageDialog(panel, "Nie można uruchomić serwera", "błąd", JOptionPane.ERROR_MESSAGE);
+                } catch(NumberFormatException exception) {
+                    JOptionPane.showMessageDialog(panel, "Niepoprawny port", "błąd", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
+
     public JPanel getPanel() {
         return panel;
     }

a56136fa7e78503128ae166d7085afa8c436397d
wojtek <wojtek.klusek2@gmail.com>
2017-08-24 14:25:24 +0200

    Person
    new class Armor
    class Person continue

--- src/main/java/warhammerrpg/Person/Person.java
+++ src/main/java/warhammerrpg/Person/Person.java
@@ -1,11 +1,48 @@
 package warhammerrpg.Person;
 
+import java.math.BigInteger;
+
 public class Person {
     //bohater
     private String name;
     private String breed;
     private String curProffesion;
     private String prevProffesion;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getCurProffesion() {
+        return curProffesion;
+    }
+
+    public void setCurProffesion(String curProffesion) {
+        this.curProffesion = curProffesion;
+    }
+
+    public String getPrevProffesion() {
+        return prevProffesion;
+    }
+
+    public void setPrevProffesion(String prevProffesion) {
+        this.prevProffesion = prevProffesion;
+    }
+
+
+
 
     //opis bohatera
     private Integer age;
@@ -19,8 +56,238 @@ public class Person {
     private String birthPlace;
     private String specialSigns;
 
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEyesColour() {
+        return eyesColour;
+    }
+
+    public void setEyesColour(String eyesColour) {
+        this.eyesColour = eyesColour;
+    }
+
+    public String getHairColour() {
+        return hairColour;
+    }
+
+    public void setHairColour(String hairColour) {
+        this.hairColour = hairColour;
+    }
+
+    public String getStarSign() {
+        return starSign;
+    }
+
+    public void setStarSign(String starSign) {
+        this.starSign = starSign;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public String getSiblings() {
+        return siblings;
+    }
+
+    public void setSiblings(String siblings) {
+        this.siblings = siblings;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getSpecialSigns() {
+        return specialSigns;
+    }
+
+    public void setSpecialSigns(String specialSigns) {
+        this.specialSigns = specialSigns;
+    }
 
 
 
+    ///Gracz
+    private String gamerName;
+    private String gameNaster;
+    private String campaign;
+    private Integer campaignYear;
+
+
+    public String getGamerName() {
+        return gamerName;
+    }
+
+    public void setGamerName(String gamerName) {
+        this.gamerName = gamerName;
+    }
+
+    public String getGameNaster() {
+        return gameNaster;
+    }
+
+    public void setGameNaster(String gameNaster) {
+        this.gameNaster = gameNaster;
+    }
+
+    public String getCampaign() {
+        return campaign;
+    }
+
+    public void setCampaign(String campaign) {
+        this.campaign = campaign;
+    }
+
+    public Integer getCampaignYear() {
+        return campaignYear;
+    }
+
+    public void setCampaignYear(Integer campaignYear) {
+        this.campaignYear = campaignYear;
+    }
+
+
+
+
+    ///Punkty Doswiadczenia
+    private BigInteger expPresent;
+    private BigInteger expAll;
+
+    public BigInteger getExpPresent() {
+        return expPresent;
+    }
+
+    public void setExpPresent(BigInteger expPresent) {
+        this.expPresent = expPresent;
+    }
+
+    public BigInteger getExpAll() {
+        return expAll;
+    }
+
+    public void setExpAll(BigInteger expAll) {
+        this.expAll = expAll;
+    }
+
+
+
+    ///Ruch w walce
+    private Integer move;
+    private Integer attackRun; ///Szarża
+    private Integer run;
+
+    public Integer getMove() {
+        return move;
+    }
+
+    public void setMove(Integer move) {
+        this.move = move;
+    }
+
+    public Integer getAttackRun() {
+        return attackRun;
+    }
+
+    public void setAttackRun(Integer attackRun) {
+        this.attackRun = attackRun;
+    }
+
+    public Integer getRun() {
+        return run;
+    }
+
+    public void setRun(Integer run) {
+        this.run = run;
+    }
+
+
+
+
+    ///Obrazek - Punkty Zbroi
+    private Integer head;
+    private Integer body;
+    private Integer rightHand;
+    private Integer leftHand;
+    private Integer rightLeg;
+    private Integer leftLeg;
+
+    public Integer getHead() {
+        return head;
+    }
+
+    public void setHead(Integer head) {
+        this.head = head;
+    }
+
+    public Integer getBody() {
+        return body;
+    }
+
+    public void setBody(Integer body) {
+        this.body = body;
+    }
+
+    public Integer getRightHand() {
+        return rightHand;
+    }
+
+    public void setRightHand(Integer rightHand) {
+        this.rightHand = rightHand;
+    }
+
+    public Integer getLeftHand() {
+        return leftHand;
+    }
+
+    public void setLeftHand(Integer leftHand) {
+        this.leftHand = leftHand;
+    }
+
+    public Integer getRightLeg() {
+        return rightLeg;
+    }
+
+    public void setRightLeg(Integer rightLeg) {
+        this.rightLeg = rightLeg;
+    }
+
+    public Integer getLeftLeg() {
+        return leftLeg;
+    }
+
+    public void setLeftLeg(Integer leftLeg) {
+        this.leftLeg = leftLeg;
+    }
 
 }

4e21d079e4dae81d9b3504137f5deb0b597e3d17
wojtek <wojtek.klusek2@gmail.com>
2017-08-24 13:45:43 +0200

    Person cd

---
+++ src/main/java/warhammerrpg/Person/Feature.java
@@ -0,0 +1,44 @@
+package warhammerrpg.Person;
+
+public class Feature {
+    private String featureName;
+    private Integer initialVal;
+    private Integer devScheme;
+    private Integer curVal;
+
+    public String getFeatureName() {
+        return featureName;
+    }
+
+    public void setFeatureName(String featureName) {
+        this.featureName = featureName;
+    }
+
+    public Integer getInitialVal() {
+        return initialVal;
+    }
+
+    public void setInitialVal(Integer initialVal) {
+        this.initialVal = initialVal;
+    }
+
+    public Integer getDevScheme() {
+        return devScheme;
+    }
+
+    public void setDevScheme(Integer devScheme) {
+        this.devScheme = devScheme;
+    }
+
+    public Integer getCurVal() {
+        return curVal;
+    }
+
+    public void setCurVal(Integer curVal) {
+        this.curVal = curVal;
+    }
+
+
+
+
+}
--- src/main/java/warhammerrpg/Person/Person.java
+++ src/main/java/warhammerrpg/Person/Person.java
@@ -1,4 +1,27 @@
 package warhammerrpg.Person;
 
 public class Person {
+    //bohater
+    private String name;
+    private String breed;
+    private String curProffesion;
+    private String prevProffesion;
+
+    //opis bohatera
+    private Integer age;
+    private String eyesColour;
+    private String hairColour;
+    private String starSign;
+    private String sex;
+    private Double weight;
+    private Double height;
+    private String siblings;
+    private String birthPlace;
+    private String specialSigns;
+
+    //cechy
+    //  glowne
+
+
+
 }

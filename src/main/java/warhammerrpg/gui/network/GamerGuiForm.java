0c0fdfb28f986c90236c980d1b09f39da1064fb2
wojtek <wojtek.klusek2@gmail.com>
2017-08-25 16:39:50 +0200

    Gamer GUI cd

--- src/main/java/warhammerrpg/gui/network/GamerGuiForm.java
+++ src/main/java/warhammerrpg/gui/network/GamerGuiForm.java
@@ -14,6 +14,17 @@ public class GamerGuiForm {
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
+    private JTextField ageTextField;
+    private JTextField sexTextField;
+    private JTextField weightTextField;
+    private JTextField heightTextField;
+    private JTextField eyesColourTextField;
+    private JTextField hairColourTextField;
+    private JTextField starSignTextField;
+    private JTextField birthPlaceTextField;
+    private JTextPane textPane1;
+    private JTextField textField4;
+    private JTextField textField5;
 
     public JPanel getPanel() {
         return panel;

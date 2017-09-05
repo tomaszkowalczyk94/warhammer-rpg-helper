878e8d875a1bc7f921a1b9a04ccacbd8b6c877f6
sandrakuc <31401348+sandrakuc@users.noreply.github.com>
2017-09-05 20:55:56 +0200

    Update UnknowObserableEventException.java

--- src/main/java/warhammerrpg/core/exception/UnknowObserableEventException.java
+++ src/main/java/warhammerrpg/core/exception/UnknowObserableEventException.java
@@ -1,9 +1,6 @@
 package warhammerrpg.core.exception;
 
 public class UnknowObserableEventException extends WarhammerRpgException {
-  public UnknowObserableEventException(Exception originalExceptionObject){
-    super(originalExceptionObject);
-  }
   
   public String toString(){
     return "Unknown observable event";

7d3f488302c1892f2a39a4553643f4da6017b7de
Tomasz Kowalczyk <tomasz.kowalczyk94@gmail.com>
2017-09-28 13:31:32 +0200

    fast fix

---
+++ src/main/java/warhammerrpg/database/entity/Fieldable.java
@@ -0,0 +1,9 @@
+package warhammerrpg.database.entity;
+
+/**
+ * Obiekt Entity który implementuje Fieldable moze byz zawarty w innych obiektach (relacja)
+ */
+public interface Fieldable {
+
+    public int getId();
+}
